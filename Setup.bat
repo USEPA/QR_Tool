b56fe9d5e60bf3aacf084ab8e53efeb1b4976ee6
mkarimiepa <Karimi.Muhammad@epa.gov>
2020-02-10 10:57:19 -0500

    This is the original SharePoint version. -Pushed to this branch for storage. -Updated README to reflect new way to install software (through Setup.bat) -Included python-3.7.4-amd64.exe needed for the Setup.bat

---
+++ Setup.bat
@@ -0,0 +1,8 @@
+python-3.7.4-amd64.exe;
+pip install pyzbar;
+pip install imutils;
+pip install qrcode;
+pip install Pillow;
+pip install opencv-python
+pip install Office365-REST-Python-Client;
+setx OPENCV_VIDEOIO_PRIORITY_MSMF 0;
--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 
 settings = {
-    'url': '',
-    'client_id': '',
-    'client_secret': ''
+    'url': 'https://usepa.sharepoint.com/sites/Emergency%20Response/EOCIncident/',
+    'client_id': '54d59940-8509-4463-b49b-5ec7126e29c5',
+    'client_secret': 'leo9tNYuo9xHbLdwcJzV1wI9BdnfvTUZcFZRg4929tE='
 }
 
 
