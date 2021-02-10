64e94621cc9ba26b36db11b476ce61d2032cc238
mkarimiepa <60198887+mkarimiepa@users.noreply.github.com>
2021-02-10 10:12:43 -0500

    Update settings.py

--- Setup/settings.py
+++ Setup/settings.py
@@ -1,5 +1,5 @@
 settings = {
-    'url': 'https://usepa.sharepoint.com/sites/Emergency%20Response/EOCIncident/',
-    'client_id': '8110e191-81b2-4056-832a-f11d9f3774f2',
-    'client_secret': '8INhDhzoUIthHOL9S+s+RE8AeY+snrBrk6mGHZJcDcI='
+    'url': '',
+    'client_id': '',
+    'client_secret': ''
 }
