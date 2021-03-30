cb85bf888cfd51c85370a166bd224cf15db23923
Muhammad Karimi <karimi.muhammad@epa.gov>
2021-03-30 12:58:57 -0400

    1. Updated settings.py file, adding several SharePoint related variables to it so that the user can change them as they wish 2. Made those same updates in the .py file, so that changed settings.py file variables are also changed in the .py since it now pulls from the settings.py file. 3. Also adjusted some text/comments and code based on the above changes 4. Renamed and moved another variable 'list_title' to 'mainDirectory' and put it in the same place as the other SharePoint variables as well as adding it to the settings.py file.
    5. Updated the User Guide to reflect the changes made to the settings.py file and QR-Toolbox.py file.
    6. Also changed an unrelated comment to be more accurate, and adjusted some formatting

--- Setup/settings.py
+++ Setup/settings.py
@@ -1,5 +1,11 @@
 settings = {
     'url': '',
     'client_id': '',
-    'client_secret': ''
+    'client_secret': '',
+    'listTitle': 'QR Timestamps',
+    'qrfolder': 'QRCodes',
+    'bkcsvfolder': 'HXWTEST',
+    'mainDirectory': 'EOC Documents',
+    'localQRBatchFile': 'names.csv',
+    'relative_url': '/sites/Emergency%20Response/EOCIncident/EOC%20Documents/QRCodes/names.csv'
 }
