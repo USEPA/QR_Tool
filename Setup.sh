b9c5c3623659ee1f77e51ec29d0f26b0ddd59673
DeaganJordan <deagan.jordan@epa.gov>
2021-12-30 11:33:21 -0500

    updated setup step

--- Setup.sh
+++ Setup.sh
@@ -4,7 +4,7 @@ alias pip="pip3"
 pip install virtualenv
 virtualenv env
 source env/bin/activate
-curl https://sh.rustup.rs -sSf | sh
+curl https://sh.rustup.rs -sSf | sh -s -- -y
 sudo apt-get install libffi-dev
 sudo apt-get install libssl-dev
 pip install -r requirements.txt
