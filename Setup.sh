c8beaccf9cb2bce1aa8bab331ad500a07b5e9855
DeaganJordan <deagan.jordan@epa.gov>
2022-03-09 10:10:37 -0500

    Updated shell script

--- Setup.sh
+++ Setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-source Setup_python.sh
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install libffi-dev
+sudo apt-get install libssl-dev
+sudo apt-get install libatlas-base-dev -y
+sudo apt-get install cmake -y
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade numpy
+python3 -m pip install virtualenv
 virtualenv env
 source env/bin/activate
 curl https://sh.rustup.rs -sSf | sh -s -- -y
-sudo apt-get install libffi-dev
-sudo apt-get install libssl-dev
 python -m pip install -r requirements.txt
 source QR_Tool.sh
